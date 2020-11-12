8e2f8d349db9fa8a91f3dc6393ca83eb89935335
agarcluq <agarcluq@everis.com>
2020-11-12 13:58:07 +0100

    Acabado

--- index.ts
+++ index.ts
@@ -1,11 +1,46 @@
-// En esta práctica debéis crear una interfaz con el nombre "UserData" en el archivo "user-data.ts". En esta interfaz, debéis añadir las propiedades "name", "age" y "phone" con los tipos cadena de texto, número y número respectivamente. Tenéis que exportar esta interfaz.
+// En esta práctica debéis crear una interfaz con el nombre "UserData" en el archivo "user-data.ts". En esta interfaz, debéis añadir las propiedades "name", "age" y "phone" con los tipos cadena de texto, número y número respectivamente. Tenéis que exportar esta interfaz. OK
 
-// En el documento "index.ts" debéis importar la interfaz que habéis creado en el punto anterior. Cread una variable llamada "user" que sea del tipo de la interfaz que habéis importado.
+// En el documento "index.ts" debéis importar la interfaz que habéis creado en el punto anterior. Cread una variable llamada "user" que sea del tipo de la interfaz que habéis importado. ok
 
-// Luego, debéis crear una función llamada "getData" que reciba un tipo genérico, un parámetro del tipo genérico (T) y que saque por consola ese dato.NO RETURN
+// Luego, debéis crear una función llamada "getData" que reciba un tipo genérico, un parámetro del tipo genérico (T) y que saque por consola ese dato.NO RETURN ok
 
 // Para terminar, asignad un objeto literal (sin clase) a la variable "user" (con los datos que queráis pero que no dé erroes de tipo) y luego realizad dos llamadas a "getData". La primera llamada debe recibir por parámetros el valor de la propiedad "name" del usuario y la segunda el valor de "phone".
 
 // OPCIONAL: Dentro de "getData" añadid un condicional con una guarda para diferenciar cuando reciba un tipo "number" y sacar por consola mensajes distintos para las dos llamadas anteriormente mencionadas.
 // Ánimo que ya lo tenéis!
 
+///////////////MI EJERCICIO//////////////////////
+
+//1. En el documento "index.ts" debéis importar la interfaz que habéis creado en el punto anterior. Cread una variable llamada "user" que sea del tipo de la interfaz que habéis importado. 
+import { UserData } from "./user-data";
+let user: UserData;
+//2.Asignad un objeto literal (sin clase) a la variable "user"
+user = {
+  name: "juan",
+  age: 18,
+  phone: 954253278
+};
+
+//5.OPCIONAL 
+function isNumber(x: any): x is number {
+  return typeof x === "number";
+}
+function isString(x: any): x is string {
+  return typeof x === "string";
+}
+
+//3.Luego, debéis crear una función llamada "getData" que reciba un tipo genérico, un parámetro del tipo genérico (T) y que saque por consola ese dato.NO RETURN + OPCIONAL 
+function getData<T>(arg: T| number| string): void {
+  if(isNumber(arg)){
+  console.log("Procedamos a añadir un tipo numero:")
+  console.log(arg);
+  }else{
+  console.log("Procedamos a añadir un tipo string:")
+  console.log(arg);
+  }
+  
+}
+
+//4.y luego realizad dos llamadas a "getData". La primera llamada debe recibir por parámetros el valor de la propiedad "name" del usuario y la segunda el valor de "phone".
+let a = getData<String>("-"+user.name);
+let b = getData<number>("-"+user.phone);
